c253cf388bae6d92cf2730271dad00c4a188d0d3
eepy <eepy@catgirlforest.com>
2026-06-28 01:48:00 -0500

    added create_file

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,28 @@ use std::path::{Path, PathBuf};
 use std::fs::File;
 use std::io::{self, Read, Write, Error};
 
+fn create_md(filename: &str) -> Result<(), Error> {
+    todo!();
+}
+
+fn create_file(filename: &str, extension: &str) -> Result<(), Error> {
+    let input_dir = PathBuf::from("src");
+    let output_dir = PathBuf::from("dst");
+
+    let output_path = output_dir.join(format!("{}.{}", filename, extension));
+
+    std::fs::create_dir_all(output_dir)?;
+
+    let mut output = File::create(&output_path)?;
+
+    let file = format!("{}.{}", filename, extension);
+    let input_path = input_dir.join(file);
+    let mut input = File::open(&input_path)?;
+    io::copy(&mut input, &mut output)?;
+
+    Ok(())
+}
+
 fn create_xhtml(filename: &str) -> Result<(), Error> {
     let input_dir = PathBuf::from("src");
     let output_dir = PathBuf::from("dst");
@@ -28,9 +50,9 @@ fn sort_file(filename: &str, extension: &str) {
     match extension {
         "xhtml" => create_xhtml(filename),
         "md" => create_md(filename),
-        "rs" => (),
-        _ => create_file(filename)
-    }
+        "rs" => Ok(()),
+        _ => create_file(filename, extension)
+    };
 }
 
 fn split_file(path: &Path) -> Option<(&str, &str)> {
