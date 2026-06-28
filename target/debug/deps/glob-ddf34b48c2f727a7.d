b944d9458343aa52f578ac8898073db21c261ec9
eepy <eepy@catgirlforest.com>
2026-06-28 01:33:01 -0500

    oops forgot to ignore target/

--- target/debug/deps/glob-ddf34b48c2f727a7.d
+++
@@ -1,7 +0,0 @@
-/var/home/eepy/Projects/ssg-rs/target/debug/deps/glob-ddf34b48c2f727a7.d: /home/eepy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/glob-0.3.3/src/lib.rs
-
-/var/home/eepy/Projects/ssg-rs/target/debug/deps/libglob-ddf34b48c2f727a7.rlib: /home/eepy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/glob-0.3.3/src/lib.rs
-
-/var/home/eepy/Projects/ssg-rs/target/debug/deps/libglob-ddf34b48c2f727a7.rmeta: /home/eepy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/glob-0.3.3/src/lib.rs
-
-/home/eepy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/glob-0.3.3/src/lib.rs:
--- target/debug/ssg-rs.d
+++
@@ -1,1 +0,0 @@
-/var/home/eepy/Projects/ssg-rs/target/debug/ssg-rs: /var/home/eepy/Projects/ssg-rs/src/main.rs
